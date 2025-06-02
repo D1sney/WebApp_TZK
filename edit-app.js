c115a62c4a08e133b747825f8da54785ca8d874b
D1sney <ivan36samsonov@gmail.com>
2025-06-02 16:13:32 +0300

    add connect with api for edit-app.js

--- edit-app.js
+++ edit-app.js
@@ -8,7 +8,7 @@ const EditForm = {
             </div>
             
             <div class="form-content">
-                <h1 class="form-title">Редактирование данных дефектов</h1>
+                <h1 class="form-title">Редактирование данных отдела</h1>
                 
                 <div v-if="loading" class="loading-message">
                     Загрузка данных...
@@ -19,139 +19,60 @@ const EditForm = {
                     <button @click="loadData" class="retry-button">Попробовать снова</button>
                 </div>
                 
-                <form v-else @submit.prevent="submitForm">
-                    <div class="data-table">
-                        <div class="table-header">
-                            <div class="header-cell name-column">Наименование</div>
-                            <div class="header-cell old-value-column">Старое значение</div>
-                            <div class="header-cell new-value-column">Новое значение</div>
-                        </div>
-                        
-                        <div class="table-row">
-                            <div class="cell name-cell">Тип фундамента (тзк)</div>
-                            <div class="cell old-value-cell">{{ oldData.foundationType || 'Не указано' }}</div>
-                            <div class="cell new-value-cell">
-                                <select v-model="newData.foundationType" class="edit-select">
-                                    <option value="">Выберите тип фундамента</option>
-                                    <option v-for="type in foundationTypes" :key="type" :value="type">
-                                        {{ type }}
-                                    </option>
-                                </select>
-                            </div>
-                        </div>
-                        
-                        <div class="table-row">
-                            <div class="cell name-cell">Тип стен (тзк)</div>
-                            <div class="cell old-value-cell">{{ oldData.wallType || 'Не указано' }}</div>
-                            <div class="cell new-value-cell">
-                                <select v-model="newData.wallType" class="edit-select">
-                                    <option value="">Выберите тип стен</option>
-                                    <option v-for="type in wallTypes" :key="type" :value="type">
-                                        {{ type }}
-                                    </option>
-                                </select>
-                            </div>
-                        </div>
-                        
-                        <div class="table-row">
-                            <div class="cell name-cell">Тип колонн (тзк)</div>
-                            <div class="cell old-value-cell">{{ oldData.columnType || 'Не указано' }}</div>
-                            <div class="cell new-value-cell">
-                                <select v-model="newData.columnType" class="edit-select">
-                                    <option value="">Выберите тип колонн</option>
-                                    <option v-for="type in columnTypes" :key="type" :value="type">
-                                        {{ type }}
-                                    </option>
-                                </select>
-                            </div>
-                        </div>
-                        
-                        <div class="table-row">
-                            <div class="cell name-cell">Тип перекрытий (тзк)</div>
-                            <div class="cell old-value-cell">
-                                {{ Array.isArray(oldData.floorType) ? oldData.floorType.join(', ') : (oldData.floorType || 'Не указано') }}
-                            </div>
-                            <div class="cell new-value-cell">
-                                <div class="custom-select-container" @click.stop>
-                                    <div class="custom-select" @click="toggleFloorDropdown">
-                                        <div class="select-display">
-                                            <span v-if="newData.floorType.length === 0" class="placeholder">
-                                                Выберите тип перекрытий
-                                            </span>
-                                            <span v-else class="selected-values">
-                                                {{ newData.floorType.join(', ') }}
-                                            </span>
-                                        </div>
-                                        <div class="select-arrow" :class="{ 'open': isFloorDropdownOpen }">▼</div>
-                                    </div>
-                                    
-                                    <div v-if="isFloorDropdownOpen" class="custom-dropdown">
-                                        <div v-for="type in floorTypes" :key="type" class="dropdown-item">
-                                            <input 
-                                                type="checkbox" 
-                                                :id="'edit-floor-' + type"
-                                                :value="type"
-                                                v-model="newData.floorType"
-                                                class="dropdown-checkbox"
-                                            >
-                                            <label :for="'edit-floor-' + type" class="dropdown-label">
-                                                {{ type }}
-                                            </label>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        
-                        <div class="table-row">
-                            <div class="cell name-cell">Тип крыши/покрытий</div>
-                            <div class="cell old-value-cell">
-                                {{ Array.isArray(oldData.roofType) ? oldData.roofType.join(', ') : (oldData.roofType || 'Не указано') }}
-                            </div>
-                            <div class="cell new-value-cell">
-                                <div class="checkbox-list-small">
-                                    <div v-for="type in roofTypes" :key="type" class="checkbox-item-small">
-                                        <input 
-                                            type="checkbox" 
-                                            :id="'edit-roof-' + type"
-                                            :value="type"
-                                            v-model="newData.roofType"
-                                            class="checkbox-input-tiny"
-                                        >
-                                        <label :for="'edit-roof-' + type" class="checkbox-label-tiny">
-                                            {{ type }}
-                                        </label>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                <div v-else>
+                    <div class="info-block">
+                        <div><b>ID:</b> {{ oldData.id }}</div>
+                        <div><b>Наименование:</b> {{ oldData.nazvanie }}</div>
                     </div>
-                    
-                    <div class="checkbox-container">
-                        <input 
-                            type="checkbox" 
-                            id="verification"
-                            v-model="isVerified"
-                            class="checkbox-input"
+                    <form @submit.prevent="submitForm">
+                        <div class="data-table">
+                            <div class="table-header">
+                                <div class="header-cell name-column">Наименование</div>
+                                <div class="header-cell old-value-column">Старое значение</div>
+                                <div class="header-cell new-value-column">Новое значение</div>
+                            </div>
+                            <div class="table-row">
+                                <div class="cell name-cell">Готовность</div>
+                                <div class="cell old-value-cell">{{ oldData.gotovnost || 'Не указано' }}</div>
+                                <div class="cell new-value-cell">
+                                    <input v-model="newData.gotovnost" class="edit-input" type="text" placeholder="Новое значение">
+                                </div>
+                            </div>
+                            <div class="table-row">
+                                <div class="cell name-cell">Количество рабочих</div>
+                                <div class="cell old-value-cell">{{ oldData.count_rabochie || 'Не указано' }}</div>
+                                <div class="cell new-value-cell">
+                                    <input v-model="newData.count_rabochie" class="edit-input" type="text" placeholder="Новое значение">
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <div class="checkbox-container">
+                            <input 
+                                type="checkbox" 
+                                id="verification"
+                                v-model="isVerified"
+                                class="checkbox-input"
+                            >
+                            <label for="verification" class="checkbox-label">
+                                Я проверил все значения и подтверждаю правильность изменений
+                            </label>
+                        </div>
+                        
+                        <button 
+                            type="submit" 
+                            class="submit-button"
+                            :class="{ enabled: isVerified }"
+                            :disabled="!isVerified"
                         >
-                        <label for="verification" class="checkbox-label">
-                            Я проверил все значения и подтверждаю правильность изменений
-                        </label>
-                    </div>
-                    
-                    <button 
-                        type="submit" 
-                        class="submit-button"
-                        :class="{ enabled: isVerified }"
-                        :disabled="!isVerified"
-                    >
-                        Отправить изменения
-                    </button>
-                    
-                    <div v-if="submitMessage" :class="submitMessageClass">
-                        {{ submitMessage }}
-                    </div>
-                </form>
+                            Отправить изменения
+                        </button>
+                        
+                        <div v-if="submitMessage" :class="submitMessageClass">
+                            {{ submitMessage }}
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     `,
@@ -162,61 +83,9 @@ const EditForm = {
             error: null,
             oldData: {},
             newData: {
-                foundationType: '',
-                wallType: '',
-                columnType: '',
-                floorType: [],
-                roofType: []
+                gotovnost: '',
+                count_rabochie: ''
             },
-            
-            isFloorDropdownOpen: false,
-            
-            foundationTypes: [
-                'Нет подходящего описания',
-                'Отсутствует',
-                'Ленточный бутовый',
-                'Ленточные из блоков ФБС',
-                'Фундаменты столбчатые стаканного типа',
-                'Плитный',
-                'Монолитная ж/б плита'
-            ],
-            
-            wallTypes: [
-                'Нет подходящего описания',
-                'Отсутствует',
-                'Крупные железобетонные блоки',
-                'Железобетонные панели',
-                'Силикатный кирпич',
-                'Керамический кирпич',
-                'Деревянные каркасно-щитовые',
-                'Деревянные рубленые брусчатые и бревенчатые'
-            ],
-            
-            columnTypes: [
-                'Нет подходящего описания',
-                'Отсутствует',
-                'Силикатный кирпич',
-                'Керамический кирпич',
-                'Сборные'
-            ],
-            
-            floorTypes: [
-                'Нет подходящего описания',
-                'Отсутствует',
-                'Деревянные',
-                'Сборные железобетонные типа ПК',
-                'Сборные железобетонные настильные',
-                'Монолитные'
-            ],
-            
-            roofTypes: [
-                'Нет подходящего описания',
-                'Отсутствует',
-                'Совмещенная железобетонная',
-                'Чердачная',
-                'Деревянная стропильная'
-            ],
-            
             isVerified: false,
             submitMessage: '',
             submitMessageClass: ''
@@ -225,12 +94,6 @@ const EditForm = {
     
     mounted() {
         this.loadData();
-        // Закрытие dropdown при клике вне его
-        document.addEventListener('click', this.closeFloorDropdown);
-    },
-    
-    beforeUnmount() {
-        document.removeEventListener('click', this.closeFloorDropdown);
     },
     
     methods: {
@@ -239,13 +102,10 @@ const EditForm = {
             this.error = null;
             
             try {
-                // Здесь укажите ваш HTTPS адрес для получения данных
-                const response = await fetch('https://your-api-endpoint.com/get-defect-data', {
-                    method: 'GET',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    }
-                });
+                // Получаем id из query-параметра ?id=...
+                const urlParams = new URLSearchParams(window.location.search);
+                const id = urlParams.get('id') || 679190;
+                const response = await fetch(`http://127.0.0.1:8000/kom_otdel/${id}`);
                 
                 if (!response.ok) {
                     throw new Error('Ошибка при загрузке данных');
@@ -254,29 +114,17 @@ const EditForm = {
                 const data = await response.json();
                 this.oldData = data;
                 
-                // Инициализируем новые данные старыми значениями
                 this.newData = {
-                    foundationType: data.foundationType || '',
-                    wallType: data.wallType || '',
-                    columnType: data.columnType || '',
-                    floorType: Array.isArray(data.floorType) ? [...data.floorType] : [],
-                    roofType: Array.isArray(data.roofType) ? [...data.roofType] : []
+                    gotovnost: data.gotovnost || '',
+                    count_rabochie: data.count_rabochie || ''
                 };
                 
             } catch (error) {
                 console.error('Ошибка загрузки:', error);
-                this.error = 'Не удалось загрузить данные. Проверьте подключение к интернету.';
+                this.error = 'Не удалось загрузить данные. Проверьте подключение к серверу.';
             } finally {
                 this.loading = false;
             }
-        },
-        
-        toggleFloorDropdown() {
-            this.isFloorDropdownOpen = !this.isFloorDropdownOpen;
-        },
-        
-        closeFloorDropdown() {
-            this.isFloorDropdownOpen = false;
         },
         
         async submitForm() {
@@ -285,16 +133,14 @@ const EditForm = {
             }
             
             try {
-                // Здесь укажите ваш HTTPS адрес для отправки изменений
-                const response = await fetch('https://your-api-endpoint.com/update-defects', {
+                const urlParams = new URLSearchParams(window.location.search);
+                const id = urlParams.get('id') || 679190;
+                const response = await fetch(`http://127.0.0.1:8000/kom_otdel/${id}`, {
                     method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
+                    headers: { 'Content-Type': 'application/json' },
                     body: JSON.stringify({
-                        oldData: this.oldData,
-                        newData: this.newData,
-                        updatedAt: new Date().toISOString()
+                        gotovnost: this.newData.gotovnost,
+                        count_rabochie: this.newData.count_rabochie
                     })
                 });
                 
